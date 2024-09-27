4c642ea04b35c26bb233762ca18bbf3cf0d9177a
sameersuman573 <sameer573cd@gmail.com>
2024-09-27 22:06:18 +0530

    Made the UI of BOt

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import ChatbotUI from './Pages/ChatBot';
+ 
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+           <Route path="/" element={<ChatbotUI />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
